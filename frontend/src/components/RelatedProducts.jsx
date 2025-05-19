f38ab936a1e4f219061c77ab8f93412a4945188e
Mudasir211 <154220597+Mudasir211@users.noreply.github.com>
2025-05-19 20:47:28 +0500

    Update RelatedProducts.jsx

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -4,7 +4,9 @@ import { json, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { useGetProductsQuery } from '../services/createApi'
 import { useGetReviewsQuery } from '../services/createApi'
+import { useParams } from 'react-router-dom';
 function RelatedProducts({category}) {
+    const {productId} = useParams()
     const { refetch } = useGetReviewsQuery(productId);
     const {isfetching,data=[]} = useGetProductsQuery()
     const productData = data
