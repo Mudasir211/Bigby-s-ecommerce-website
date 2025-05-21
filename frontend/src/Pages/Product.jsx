acef5b2daf0259211abad73a805b31a196c8b862
Mudasir211 <154220597+Mudasir211@users.noreply.github.com>
2025-05-21 17:15:07 +0500

    Update Product.jsx

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { assets, products } from "../assets/frontend_assets/assets"
 import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
@@ -16,7 +16,7 @@ import AverageStars from "../components/AverageStars"
 
 function Product() {
  
-
+    const navigate = useNavigate()
     const {productId} = useParams()
   
 
@@ -91,10 +91,16 @@ const fetchData = () =>{
   useEffect(() => {
     if (productData.length > 0) {
         fetchData();
+        refetch()
       }
-refetch()
   }, [productId,data])
- 
+  
+const directOrder = ()=>{
+    selectSize.length>0 &&  dispatch(actions.addOrderItem({productId:product._id,name:product.name,price: product.price,size: selectSize,imgUrl:product.image[0],quantity: +1,_id:nanoid()} )) 
+   selectSize.length>0 && navigate("/place-order")
+    
+}   
+   
   const add =()=>{
     selectSize.length<=0 && notify()
         if (isSuccess) {
@@ -124,11 +130,12 @@ refetch()
   <p className=" opacity-70">{product.description}</p>
   </div>
     
-<div className="mb-10 space-y-5"><h1>Select Size</h1><div>{product.sizes.map((size,i)=><button onClick={(e)=>setSelectSize(size)} key={i} className={`p-2 px-4 ${size===selectSize && 'border border-orange-400'} mr-2 bg-gray-100 `}>{size}</button>)}</div>
-<button onClick={add} className="py-3 text-sm text-white bg-black px-7">ADD TO CART</button>
+<div className="mb-10 space-y-5"><h1>Select Size</h1><div>{product.sizes.map((size,i)=><button onClick={(e)=>setSelectSize(size)} key={i} className={`p-2 px-4 ${size===selectSize && 'border border-orange-400'} mr-2 bg-gray-100 `}>{size}</button>)}</div> <div className="space-y-1.5">
+<button onClick={add} className="py-3 text-sm text-white bg-black px-7">ADD TO CART</button><br /><button onClick={directOrder} className="w-[146.98px] py-2.5 text-sm text-white bg-yellow-300 px-7">BUY NOW</button></div>
+
 <div className="flex flex-col gap-1 py-5 mt-5 text-sm text-gray-500 border-t border-gray-300"><p>100% Original product.</p><p>Cash on delivery is available on this product.</p><p>Easy return and exchange policy within 7 days.</p></div>
 </div></div></div>
-<div className="my-0 sm:my-5"><div className="flex"><p className={`px-5 cursor-pointer py-3 text-sm border ${!option && "border-[#00000073]"}`} onClick={()=>setOption(false)}>Description</p><p className={`px-5 py-3 text-sm border ${option && "border-[#00000073]"} cursor-pointer`} onClick={()=>setOption(true)}>Reviews ({totalReviews})</p></div>{!option ? <div className="flex flex-col gap-4 px-6 py-6 text-sm text-gray-500 border"><p>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.</p><p>E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.</p></div> : <ReviewsList/>}</div>
+<div className="my-20 sm:my-5"><div className="flex"><p className={`px-5 cursor-pointer py-3 text-sm border ${!option && "border-[#00000073]"}`} onClick={()=>setOption(false)}>Description</p><p className={`px-5 py-3 text-sm border ${option && "border-[#00000073]"} cursor-pointer`} onClick={()=>setOption(true)}>Reviews ({totalReviews})</p></div>{!option ? <div className="flex flex-col gap-4 px-6 py-6 text-sm text-gray-500 border"><p>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.</p><p>E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.</p></div> : <ReviewsList/>}</div>
 <RelatedProducts category={product.category}/>
     </div>
 
